add25b2b06c39736ac4cf4c8f66e2bec5a8f8e66
Chris Williamson <chriswill0629@gmail.com>
2025-04-05 11:17:54 -0400

    Working on navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import './App.css'
-import {HStack, Button} from "@chakra-ui/react";
+import {Grid, GridItem} from "@chakra-ui/react";
+import NavBar from "@/components/NavBar.tsx";
 
 function App() {
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <Grid templateAreas={{
+            base: `"nav" "main"`,
+            lg: `"nav nav" "aside main"`
+        }}>
+            <GridItem area="nav">
+                <NavBar/>
+            </GridItem>
+            <GridItem area="aside" bg="gold" display={{base: "none", lg: "block"}}>aside</GridItem>
+            <GridItem area="main" bg="blue">main</GridItem>
+        </Grid>
+    )
 }
 
 export default App
