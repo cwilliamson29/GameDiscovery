e8681e75ec7a9eabc2493bb42f4b7d853e2d0687
Chris Williamson <chriswill0629@gmail.com>
2025-04-05 23:46:03 -0400

    Implementing environment variables for api key

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,18 +1,35 @@
 import {useEffect, useState} from 'react'
 import apiClient from "@/services/apiClient.ts";
 
+interface Game {
+    id: number
+    name: string
+    released: string
+}
+
+interface FetchGamesResponse {
+    count: number
+    results: Game[]
+}
+
 function GameGrid() {
-    const [games, setGames] = useState([])
+    const [games, setGames] = useState<Game[]>([])
     //const [error, setError] = useState('')
 
     useEffect(() => {
-        console.log(import.meta.env.VITE_API_KEY)
-        apiClient.get("/games")
-            .then(res => setGames(res.data))
+        //console.log(import.meta.env.VITE_API_KEY)
+        apiClient.get<FetchGamesResponse>("/games")
+            .then(res => setGames(res.data.results))
             .catch(err => console.log(err))
     }, []);
     return (
-        <div>{games}</div>
+        <div>
+            {games.map((game) => (
+                <div key={game.id}>
+                    {game.name}
+                </div>
+            ))}
+        </div>
     )
 }
 
